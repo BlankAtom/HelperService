764c9ac2c3c005174c089fb4898714be8a60b086
Rocael <rocael@qq.com>
2023-02-23 18:14:27 +0800

    Add Timer init.

--- ETWHost.Core/Mission/MissionManager.cs
+++ ETWHost.Core/Mission/MissionManager.cs
@@ -1,7 +1,24 @@
-﻿namespace ETWHost.Core.Mission;
+﻿using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace ETWHost.Core.Mission;
 
 public class MissionManager : IMissionManager
 {
-    
+    private Timer _centerTimer;
+
+    private DateTime _nowTime;
+
+    public MissionManager()
+    {
+        initTimer();
+    }
+
+    private void initTimer()
+    {
+        _centerTimer = new Timer(5000);
+        _centerTimer.Elapsed += (_, _) => _nowTime = DateTime.Now;
+        _centerTimer.Start();
+    }
 }
 
